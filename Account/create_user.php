d48d6c16e8ec3251e36dc01b93f28e77bf01f1e6
--global <--global>
2021-02-15 08:50:02 -0500

    Creation of createuser/DV branch

---
+++ Account/create_user.php
@@ -0,0 +1,21 @@
+<?php
+    //Code to connect to DB (Will include this once we have a database to test with)
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Create User</title>
+
+</head>
+<body>
+    
+
+</body>
+
+</html>
