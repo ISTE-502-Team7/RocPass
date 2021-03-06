35b0976ce7fe782de32dd7a64a6f0e35441ea020
--global <--global>
2021-03-05 23:17:06 -0500

    added d3.js lib and the structure of the DV code

---
+++ App/DV/barchart.js
@@ -0,0 +1,26 @@
+const data = [
+    { name: 'Bob', address: '69 Boob Road', Zip: 13408, email: 'bob@bob.com', first_time: 'false' },
+    { name: 'Joe', address: 'Tha Crossroads', Zip: 14623, email: 'Joe@bob.com', first_time: 'false' },
+    { name: 'Tony', address: '24 Fairwood Drive', Zip: 14623, email: 'Tony123@rit.edu', first_time: 'true' }
+];
+
+// Calculate number of visitors in a zipcode
+
+// What percentage of those are first time visitors?
+
+// What happens to the number of visitors over time? (line graph)
+
+
+// SVG container to hold visualization
+const svg = d3.select('#example').append('svg') // select element by ID and append svg
+    .attr('width', 500)
+    .attr('height', 500)
+    .style('backgroud-color', '#666666');
+
+
+
+// Create visualization down here (Bar chart, line graph, geo plot, etc...)
+
+
+
+
